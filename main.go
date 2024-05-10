e581969b8710a43c1ce9f1784f497536b7fed12e
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-05-10 11:54:23 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,119 +1,3 @@
-// package main
-
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-// )
-
-// func main() {
-// 	 sum := 0
-// 	// average := 0
-// 	// median := 0
-// 	// variance := 0
-// 	// stnddev := 0
-
-// 	inputfilename := os.Args[1]
-// 	inputfile, err := os.Open(inputfilename)
-// 	if err != nil {
-// 		fmt.Println("Error opening file ", err)
-// 		return
-// 	}
-
-// 	scanner := bufio.NewScanner(inputfile)
-// 	//var new string
-// 	//var line2 []int
-// 	var splitted []string
-
-// 	for scanner.Scan(){
-// 		new := scanner.Text()
-// 		// fmt.Println(len(new))
-// 		splitted = strings.Split(new, "\n")
-// 		// fmt.Println(splitted)
-
-// 		for i := 0; i < len(splitted); i++{
-// 			atc, err := strconv.Atoi(string(splitted[i]))
-// 			if err != nil{
-// 				fmt.Println(err)
-// 			}
-// 			sum += atc
-// 		}
-// 	}
-// 	count1 := 0
-// 	for i, char := range splitted{
-// 		fmt.Println(char[i])
-// 		count1++
-// 	}
-// 	// fmt.Println(count1)
-
-// 	fmt.Println(sum)
-
-// 	// Average(sum, )
-
-// }
-
-// func Average(sum int, length int) int{
-// 	ave := sum / length
-// 	return ave
-// }
-
-// ________________________________________________________________
-
-// package main
-
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-// )
-
-// func main() {
-// 	 sum := 0
-// 	// average := 0
-// 	// median := 0
-// 	// variance := 0
-// 	// stnddev := 0
-
-// 	inputfilename := os.Args[1]
-// 	inputfile, err := os.Open(inputfilename)
-// 	if err != nil {
-// 		fmt.Println("Error opening file ", err)
-// 		return
-// 	}
-
-// 	scanner := bufio.NewScanner(inputfile)
-// 	//var new string
-// 	//var line2 []int
-
-// 	for scanner.Scan(){
-// 		new := scanner.Text()
-// 		splitted := strings.Split(new, "\n")
-
-// 		sum = Sum(splitted)
-
-// 	}
-// 	fmt.Println(sum)
-
-// }
-
-// func Sum(splitted []string) int{
-// 	sum := 0
-// 	for i := 0; i < len(splitted); i++{
-// 		atc, err := strconv.Atoi(string(splitted[i]))
-// 		if err != nil{
-// 			fmt.Println(err)
-// 		}
-// 		sum += atc
-// 	}
-// 	return sum
-// }
-
-// ________________________________________________________________
-
 package main
 
 import (
@@ -121,14 +5,16 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	maths"maths_skills/maths"
 )
 
 func main() {
-	sum := 0
-	// average := 0
-	// median := 0
-	// variance := 0
-	// stnddev := 0
+	var sum float64
+
+	if len(os.Args) != 2 {
+		fmt.Println("Error: Incorrect number of arguments")
+		return
+	}
 
 	inputfilename := os.Args[1]
 	inputfile, err := os.Open(inputfilename)
@@ -136,56 +22,52 @@ func main() {
 		fmt.Println("Error opening file ", err)
 		return
 	}
+	defer inputfile.Close()
+
+	fileinfo, err := inputfile.Stat()
+	if err != nil{
+		fmt.Println("Error: ",err)
+		return
+	}
+	if fileinfo.Size() == 0{
+		fmt.Println("Error: File is empty")
+		return
+	}
 
 	scanner := bufio.NewScanner(inputfile)
 
-	var data []int
+
+	var data []float64
 
 	for scanner.Scan() {
 		new := scanner.Text()
 
-		atc, err := strconv.Atoi(new)
+		atc, err := strconv.ParseFloat(new, 64)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error: ",err)
+			return
 		}
-		data = append(data, atc)
+		data = append(data, float64(atc))
 
 	}
+	if err := scanner.Err() ; err != nil{
+		fmt.Println("Error: ",err)
+		return
+	} 
 	for i := 0; i < len(data); i++ {
 		sum += data[i]
 	}
-	length := len(data)
+	length := float64(len(data))
 	fmt.Println(sum)
 
-	// fmt.Println(len(data))
-	// fmt.Println(data[3])
 
-	average := Average(sum, length)
-	median := Median(data)
-	fmt.Println(average)
-	fmt.Println(median)
+	average := maths.Average(float64(sum), length)
+	median := maths.Median(data)
+	variance := maths.Variance(data, float64(average))
+	stddev := maths.StdDev(variance)
+	fmt.Println("Average:",int(average))
+	fmt.Println("Median:",int(median))
+	fmt.Println("Variance:",int(variance))
+	fmt.Println("Standard Deviation:",int(stddev))
 }
 
-func Average(sum int, length int) int {
-	ave := sum / length
-	return ave
-}
-
-func Median(data []int) int {
-	for i := 0; i < len(data); i++ {
-        for j := i + 1; j < len(data); j++ {
-            if data[i] > data[j] {
-                data[i], data[j] = data[j], data[i]
-            }
-        }
-    }
-	var median int
-	mid := len(data) / 2
-
-	if len(data)%2 == 0 {
-		median = (data[mid] + data[mid-1]) / 2
-	} else {
-		median = data[mid]
-	}
-	return median
-}
